3a9267cc6d9cb6f34be06d4afe35d6287737d272
jorgee16 <83295557+jorgee16@users.noreply.github.com>
2026-08-29 17:29:12 +0100

    Install direct pinch zoom on MapLibre adapters

--- src/map/maplibre-touch-zoom.js
+++ src/map/maplibre-touch-zoom.js
@@ -1,3 +1,7 @@
+import {
+  MapLibrePmtilesMapAdapter
+} from './maplibre-pmtiles-map-adapter.js';
+
 export function installMapLibreTouchZoom(adapter) {
   const map = adapter?.map;
   const container = map?.getContainer?.();
@@ -5,6 +9,9 @@ export function installMapLibreTouchZoom(adapter) {
 
   Object.defineProperty(adapter, '__atlasTouchZoomInstalled', { value: true });
 
+  // Dedicate two-finger gestures to zoom. Rotation/pitch during the same
+  // gesture makes Android pinch feel heavy and is unnecessary because Atlas
+  // already owns heading-up/north-up explicitly.
   map.touchZoomRotate?.enable?.();
   map.touchZoomRotate?.disableRotation?.();
   map.touchPitch?.disable?.();
@@ -12,4 +19,23 @@ export function installMapLibreTouchZoom(adapter) {
 
   container.classList.add('atlas-direct-touch-zoom');
   return adapter;
+}
+
+const prototype = MapLibrePmtilesMapAdapter?.prototype;
+if (prototype && !prototype.__atlasDirectTouchZoomInstalled) {
+  Object.defineProperty(prototype, '__atlasDirectTouchZoomInstalled', {
+    value: true
+  });
+
+  const originalSetRegion = prototype.setRegion;
+  prototype.setRegion = async function setRegion(region, options = {}) {
+    installMapLibreTouchZoom(this);
+    return originalSetRegion.call(this, region, options);
+  };
+
+  const originalSetNavigationTravelMode = prototype.setNavigationTravelMode;
+  prototype.setNavigationTravelMode = function setNavigationTravelMode(mode = null) {
+    installMapLibreTouchZoom(this);
+    return originalSetNavigationTravelMode.call(this, mode);
+  };
 }
