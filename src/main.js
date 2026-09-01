c14094fd58a752d7ad437dc1ca59fbb405cc0fc7
jorgee16 <83295557+jorgee16@users.noreply.github.com>
2026-09-01 07:54:13 +0100

    Load driving navigation polish after legacy UI styles

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import "./ui/components/status-toast/status-toast.css";
 import "./ui/components/overflow-menu/overflow-menu.css";
 import './ui/atlas-ui-consistency.css';
 import './ui/navigation-light-contrast.css';
+import './ui/navigation-driving-polish.css';
 import * as maplibregl from 'maplibre-gl';
 import maplibreWorkerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
 import { createApp } from './app.js';
